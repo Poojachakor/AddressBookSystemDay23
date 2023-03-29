b77d72dd397badf35bad7fdd0d8a263c7a1efade
Poojachakor <chakorpooja07@gmail.com>
2023-03-29 19:51:15 +0530

    Add: UC5 file

--- AddressBookSystem2.0/Program.cs
+++ AddressBookSystem2.0/Program.cs
@@ -10,7 +10,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n22.Exit");
+                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Edit Contact \n5.Delete Contact \n22.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -22,6 +22,12 @@
                         break;
                     case 3:
                         Book.DisplayContact();
+                        break;
+                    case 4:
+                        Book.EditContact();
+                        break;
+                    case 5:
+                        Book.DeleteContact();
                         break;
                     case 22:
                         flag = false;
@@ -35,4 +41,5 @@
         }
     }
 }
+
 
